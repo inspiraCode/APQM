9fd7e68e31ebbef891641fd75f461ee2b2623d6d
alfredoPacheco <apacheco@capsonic.com>
2014-05-18 03:36:28 -0600

    RFQDetail page included

---
+++ website/RFQ/RFQDetail.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="RFQDetail.aspx.cs" Inherits="RFQ_RFQDetail" %>
+
+<%@ Register src="rfqDetailList.ascx" tagname="rfqDetailList" tagprefix="uc1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>RFQ Detail</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="RFQDetailContainer">
+        
+        <uc1:rfqDetailList ID="uscRFQDetailList" runat="server" />
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ website/RFQ/RFQDetail.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class RFQ_RFQDetail : System.Web.UI.Page
+{
+    private long eavKeyFilter = -1;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (ViewState["RFQDetail_eavKeyFilter"] == null)
+        {
+            load();
+        }
+    }
+    public void load()
+    {
+        if (retrieveEntity())
+        {
+            ViewState["RFQDetail_eavKeyFilter"] = eavKeyFilter;
+            RfqDetailCRUD rfqDetail_CRUD = new RfqDetailCRUD();
+            List<RFQDetail> rfqDetailList = rfqDetail_CRUD.readByParentID(eavKeyFilter);
+            uscRFQDetailList.setEntity(rfqDetailList);
+        }
+        else
+        {
+            uscRFQDetailList.setEntity(null);
+        }
+    }
+    private bool retrieveEntity()
+    {
+        string sEAVKey = Request["EAV_ID"];
+        if (sEAVKey != "" && sEAVKey != null)
+        {
+            eavKeyFilter = long.Parse(Request["EAV_ID"]);
+        }
+        else
+        {
+            eavKeyFilter = -1;
+        }
+        return true;
+    }
+}
